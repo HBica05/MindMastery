c9950f267a7d26583f52ccc07f7d30b92945f867
HBica05 <bicahaadiyah@gmail.com>
2024-08-11 15:43:07 +0200

    Add validation for empty user input

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 inputted = input('type your name please: ').strip().lower()
 
-
+while inputted == '':
+    inputted = input(
+        'You didnt type anything! Please input your name please: ').strip(
+        ).lower()
 print(f'Welcome to the Mind Master game {inputted} !')
 
