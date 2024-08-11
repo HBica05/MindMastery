f0bf3ea0fd97a3bd9f2eeb4bdc52e5d2a2abe1bd
HBica05 <bicahaadiyah@gmail.com>
2024-08-11 15:56:37 +0200

    Improve user request with newline for user input

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
-inputted = input('type your name please: ').strip().lower()
+inputted = input('type your name please:\n ').strip()
 
 while inputted == '':
     inputted = input(
-        'You didnt type anything! Please input your name please: ').strip(
+        'You didnt type anything! Please input your name please:\n ').strip(
         ).lower()
 print(f'Welcome to the Mind Master game {inputted} !')
 
 print(
     """
-Please enter your preferred Topic:
+Topics:
 1. General Knowledge
 2. Science
 3. Sports
@@ -16,3 +16,6 @@ Please enter your preferred Topic:
 5. Mathematics
     """
 )
+
+option_inputted = input('Select option 1 to 5:\n ').strip().lower()
+available_options = ["1", "2", "3", "4", "5"]
